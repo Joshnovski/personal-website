53bf0ef78cf63ee1289af51f0491c806cd954bda
Joshnovski <josh.morphew@hotmail.com>
2023-07-16 14:29:06 -0500

    Improved night mode with changes

--- lightDarkSwitch.js
+++ lightDarkSwitch.js
@@ -14,7 +14,15 @@ for (var i = 0; i < lightModeElements.length; i++) {
     var nav = document.getElementsByTagName("nav");
     var boxTL = document.getElementsByClassName("boxTL");
     var boxTR = document.getElementsByClassName("boxTR");
-    var boxBR = document.getElementsByClassName("boxBR")
+    var boxBR = document.getElementsByClassName("boxBR");
+
+    var aboutMeImg = document.querySelector('.aboutMeBackground');
+    var foregroundImg = document.querySelector('.foreground');
+    var backgroundImg = document.querySelector('.background');
+    var skyImg = document.querySelector('.sky');
+
+    var projectImgLeft = document.getElementsByClassName("projectImageLeft");
+    var projectImgRight = document.getElementsByClassName("projectImageRight");
 
     //Check state of Light/Dark mode
     if (isLight) {
@@ -48,6 +56,22 @@ for (var i = 0; i < lightModeElements.length; i++) {
 
       for (var j = 0; j < boxBR.length; j++) {
         boxBR[j].style.backgroundColor = "#003C30";
+      }
+
+      foregroundImg.src = 'assets/DayForest.png';
+      aboutMeImg.src = 'assets/DayForest.png';
+      aboutMeImg.style.backgroundColor = '#005335';
+      backgroundImg.src = 'assets/DayMountain.png';
+      skyImg.src = 'assets/DaySky.png';
+
+      document.documentElement.style.setProperty('--mask-color', 'rgba(0, 0, 0, 0.4)');
+
+      for (var j = 0; j < projectImgLeft.length; j++) {
+        projectImgLeft[j].style.backgroundColor = "#236e4b";
+      }
+
+      for (var j = 0; j < projectImgRight.length; j++) {
+        projectImgRight[j].style.backgroundColor = "#6DA0DA";
       }
 
       // prepare state for switch back to light mode on next click
@@ -84,6 +108,26 @@ for (var i = 0; i < lightModeElements.length; i++) {
 
       for (var j = 0; j < boxBR.length; j++) {
         boxBR[j].style.backgroundColor = "#343372";
+      }
+
+      for (var j = 0; j < foregroundImg.length; j++) {
+        foregroundImg[j].src = 'assets/NightForest.png';
+    }
+
+      foregroundImg.src = 'assets/NightForest.png';
+      aboutMeImg.src = 'assets/NightForest.png';
+      aboutMeImg.style.backgroundColor = '#1F2C33';
+      backgroundImg.src = 'assets/NightMountain.png';
+      skyImg.src = 'assets/NightSky.png';
+
+      document.documentElement.style.setProperty('--mask-color', 'rgba(0, 0, 0, 0)');
+
+      for (var j = 0; j < projectImgLeft.length; j++) {
+        projectImgLeft[j].style.backgroundColor = "#2C4344";
+      }
+
+      for (var j = 0; j < projectImgRight.length; j++) {
+        projectImgRight[j].style.backgroundColor = "#5C3D5E";
       }
 
       // prepare state for switch back to light mode on next click
